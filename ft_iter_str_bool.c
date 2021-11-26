1b135f83c1776d796aa977e7c82aa35f2a5412c1
ander-lab <jimenezs.alex@icloud.com>
2021-11-26 12:04:50 +0100

    ft_str_iteri_bool && delete ft_str_have_space

--- ft_iter_str_bool.c
+++ ft_iter_str_bool.c
@@ -6,20 +6,20 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:08:27 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/11/26 10:48:07 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/11/26 12:03:29 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_iter_str_bool(char *str, int (*f)(char))
+int	ft_iter_str_bool(char *str, int (*f)(int))
 {
 	int	aux;
 
 	aux = 0;
 	while (str[aux] != 0)
 	{
-		if ((*f)(str[aux]))
+		if (f(str[aux]))
 			return (1);
 		aux++;
 	}
