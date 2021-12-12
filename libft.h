e2f7ad03eabf7e04abb51651156f725b146f8815
ander-lab <jimenezs.alex@icloud.com>
2021-12-12 20:45:03 +0100

    count chars

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ajimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 14:13:14 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/12/12 18:52:57 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/12/12 20:43:14 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,10 +72,9 @@ char			**ft_split(char const *s, char c);
 void			ft_free_matrix(char **matrix);
 int				ft_iter_str_bool(char *str, int (*f)(int));
 char			*get_next_line(int fd);
-//int				ft_min_int(int *str);
-//int				ft_max_int(int *str);
 size_t			ft_max_int(size_t *str);
 size_t			ft_min_int(size_t *str);
+size_t			count_chars(char c, char *str);
 /*
 **Matrix Library
 */
